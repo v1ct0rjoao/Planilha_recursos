06516eac7884578410182dd3c593df31d87aca3b
Victor <joao.victormeneses@ufrpe.br>
2026-03-20 10:14:52 -0300

    atualização de url

--- lab-manager/src/utils/constants.js
+++ lab-manager/src/utils/constants.js
@@ -1,5 +1,5 @@
-//export const API_URL = 'https://planilha-recursos.onrender.com/api';
-export const API_URL = 'http://127.0.0.1:5000/api';
+export const API_URL = 'https://planilha-recursos.onrender.com/api';
+//export const API_URL = 'http://127.0.0.1:5000/api';
 
 console.log(`🔌 Sistema conectado diretamente em: ${API_URL}`);
 
