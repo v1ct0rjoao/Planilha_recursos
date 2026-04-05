14cba2b6873a5b40ee7ccfe9c6708ffe00af68c8
João Victor Gomes <109557692+v1ct0rjoao@users.noreply.github.com>
2026-04-05 11:01:42 -0300

    Correção de texto

--- lab-manager/src/components/business/LocationIcon.jsx
+++ lab-manager/src/components/business/LocationIcon.jsx
@@ -1,7 +1,7 @@
 //centraliza a lógica de "qual ícone usar para cada tipo de máquina".
 import React from 'react';
 import { Warehouse, Cpu, Thermometer } from 'lucide-react';
-// Importamos a função auxiliar que criamos lá no começo
+
 import { getLocationType } from '../../utils/helpers';
 
 const LocationIcon = ({ id, size = 20, className }) => {
